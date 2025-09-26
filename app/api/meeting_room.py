fb40a0d3db2b5638baa63f10cc885206185a85be
NOK <nok2001@mail.ru>
2025-09-26 11:03:12 +0600

    Add delete meeting room

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.db import get_async_session
 from crud.meeting_room import (create_meeting_room, get_room_id_by_name,
                                read_all_rooms_from_db, update_meeting_room,
-                               get_meeting_room_by_id)
+                               get_meeting_room_by_id, delete_meeting_room)
 from schemas.meeting_room import (MeetingRoomCreate, MeetingRoomDB,
                                   MeetingRoomUpdate)
+from models.meeting_room import MeetingRoom
+
 
 router = APIRouter(prefix="/meeting_rooms", tags=["Meeting Rooms"])
 
@@ -50,17 +52,29 @@ async def partially_update_meeting_room(
     update_data: MeetingRoomUpdate,
     session: AsyncSession = Depends(get_async_session)
 ):
-    meeting_room = await get_meeting_room_by_id(meeting_room_id, session)
-
-    if meeting_room is None:
-        raise HTTPException(status_code=404,
-                            detail="Meeting room not found!")
+    meeting_room = await check_meeting_room_exists(meeting_room_id, session)
 
     if update_data.name is not None:
         await check_name_dublicate(update_data.name, session)
 
     meeting_room = await update_meeting_room(meeting_room, update_data, session)
     return meeting_room
+
+
+@router.delete(
+    "/{meeting_room_id}",
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True
+)
+async def remove_meeting_room(
+    meeting_room_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    
+    meeting_room = await check_meeting_room_exists(meeting_room_id, session)
+    db_room = await delete_meeting_room(meeting_room, session)
+
+    return db_room
 
 async def check_name_dublicate(
     room_name: str,
@@ -72,3 +86,16 @@ async def check_name_dublicate(
     if room_id is not None:
         raise HTTPException(status_code=422,
                             detail="There is already a meeting room with what name!")
+
+
+async def check_meeting_room_exists(
+    meeting_room_id: int,
+    session: AsyncSession
+) -> MeetingRoom:
+    
+    meeting_room = await get_meeting_room_by_id(meeting_room_id, session)
+
+    if meeting_room is None:
+        raise HTTPException(status_code=404,
+                            detail="Meeting room not found!")
+    return meeting_room
