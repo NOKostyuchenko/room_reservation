029c94d97ca964d5c3e65dce04bfd3f43eb270de
NOK <nok2001@mail.ru>
2025-09-30 17:22:34 +0600

    A small fix

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.db import get_async_session
