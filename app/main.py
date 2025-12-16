33e637730ca9b9dd4c1288fb13277c8b4c03dfce
NOK <nok2001@mail.ru>
2025-12-16 16:16:20 +0600

    adding CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from uvicorn import run
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routers import main_router
 from core.configs import app_settings
 
 app = FastAPI(title=app_settings.app_title, docs_url="/swagger")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
 
 app.include_router(main_router)
 
